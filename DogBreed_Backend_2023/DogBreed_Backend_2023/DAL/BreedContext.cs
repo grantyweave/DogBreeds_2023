e7bf63e56746dec17c97c9f6bef6d41d1ee1afd7
Grant Weaver <grantyweaver@gmail.com>
2023-03-15 18:58:46 -0400

    03-15-23

---
+++ DogBreed_Backend_2023/DogBreed_Backend_2023/DAL/BreedContext.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using DogBreed_Backend_2023.Models;
+
+namespace DogBreed_Backend_2023.DAL
+{
+  public class BreedContext : DbContext
+  {
+    // Two constructors, first one is empty
+    public BreedContext()
+    {
+
+    }
+
+    // Second one injects the context options
+    public BreedContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    // Create the table based off the model
+    public DbSet<DogBreedApi> DogBreeds { get; set; }
+
+    private static IConfigurationRoot _configuration;
+
+    // Set the configuration to use the JSON file for the connection string
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      if (!optionsBuilder.IsConfigured)
+      {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+        _configuration = builder.Build();
+        string cnstr = _configuration.GetConnectionString("BreedDb");
+        optionsBuilder.UseSqlServer(cnstr);
+      }
+    }
+  }
+}

533aaa284d48b6c57cb7b570fb360b0c7e2e6cbb
Kara-Gheldof <karagheldof@gmail.com>
2023-03-22 19:47:27 -0400

    3/22 initial

--- src/app/breed/breed.module.ts
+++ src/app/breed/breed.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreedListComponent } from './breed-list/breed-list.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [
     BreedListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [
     BreedListComponent

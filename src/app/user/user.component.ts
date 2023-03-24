abb5c62660e2d7b1a9104d8fd8090c70d09fc90a
Kara-Gheldof <karagheldof@gmail.com>
2023-03-23 21:58:46 -0400

    final 3/23

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { IUser } from '../interface/user';
 
 @Component({
   selector: 'app-user',
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./user.component.css']
 })
 export class UserComponent {
+firstName: string = ""
+lastName?: string = ""
+email: string = ""
+password: string = ""
 
+addNewUser(form: NgForm) {
+  let newUser: IUser = {
+   id: -1,
+   firstName: form.form.value.firstName,
+   lastName: form.form.value.lastName,
+   email: form.form.value.email,
+   password: form.form.value.password
+  };
+}
 }
